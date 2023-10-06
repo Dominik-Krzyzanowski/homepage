a05949d013f5e0e3e1d58c51c65dc9c72238dfe2
Dominik Krzyżanowski <dominik_krzyzanowski@wp.pl>
2023-10-06 19:30:36 +0200

    js code refactoring

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-console.log("Hello! Welcome on my website.");
+{
+    const welcome = () => {
+        console.log("Hello! Welcome on my website.");
+    }
 
-let section__button = document.querySelector(".section__button"); //assign the button variable in the JS,
-let js_author = document.querySelector(".js_author")//assign the <div class=js_author> variable in the JS,
+    const hideAuthorDataClick = () => {
+        const js_author = document.querySelector(".js_author")//assign the <div class=js_author> variable in the JS,
+        js_author.classList.toggle("hidden");
+        section__button.innerText = js_author.classList.contains("hidden")
+            ? "Pokaż informacje o autorze"
+            : "Ukryj informacje o autorze"; //this instruction shows or hide informations aobut author of the website
+    };
 
-section__button.addEventListener("click", () => {
-    js_author.classList.toggle("hidden");
-    section__button.innerText = js_author.classList.contains("hidden")
-        ? "Pokaż informacje o autorze"
-        : "Ukryj informacje o autorze"; //this instruction shows or hide informations aobut author of the website
-});
+    const section__button = document.querySelector(".section__button"); //assign the button variable in the JS,
+
+    section__button.addEventListener("click", hideAuthorDataClick);
+
+    welcome();
+}
