65fda232a0bbe3e9f32d6d0439cec43b0e313d4c
Dominik Krzyżanowski <dominik_krzyzanowski@wp.pl>
2023-10-10 20:05:28 +0200

    js init function added

--- js/script.js
+++ js/script.js
@@ -3,17 +3,22 @@
         console.log("Hello! Welcome on my website.");
     }
 
-    const hideAuthorDataClick = () => {
-        const js_author = document.querySelector(".js_author")//assign the <div class=js_author> variable in the JS,
-        js_author.classList.toggle("hidden");
-        section__button.innerText = js_author.classList.contains("hidden")
+    const hideAuthorInfoClick = () => {
+        const author = document.querySelector(".author")
+        author.classList.toggle("hidden");
+
+        const innerButtonTextChange = document.querySelector(".js-button");
+        innerButtonTextChange.innerText = author.classList.contains("hidden")
             ? "Pokaż informacje o autorze"
-            : "Ukryj informacje o autorze"; //this instruction shows or hide informations aobut author of the website
+            : "Ukryj informacje o autorze";
     };
 
-    const section__button = document.querySelector(".section__button"); //assign the button variable in the JS,
+    const init = () => {
+        const hideSection = document.querySelector(".js-button");
+        hideSection.addEventListener("click", hideAuthorInfoClick);
 
-    section__button.addEventListener("click", hideAuthorDataClick);
+        welcome();
+    }
 
-    welcome();
+    init();
 }
